19a36a67dcc8453c5502dadd7469fe06526dac6f
Dawid Grabek <grabekd37@gmail.com>
2022-02-17 12:21:45 +0100

    Add and configure sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import './css/main.css'
+import './sass/main.scss'
 
 import {
   Input,
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import '../css/loader.css'
+import '../sass/components/_loader.scss'
 
 const Loader = () => {
   return (
